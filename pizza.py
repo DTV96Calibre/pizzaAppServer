3bfae832a7830de2f1a99f68bb99519a145b0b43
Daniel Vasquez <dtv002@bucknell.edu>
2017-11-02 17:15:28 -0400

    Added pizza json generation

---
+++ pizza.py
@@ -0,0 +1,25 @@
+NUM_TOPPINGS = 3
+
+class Pizza:
+    name = "New Pizza"
+    size = 1
+    toppings = [True for i in range(NUM_TOPPINGS)]
+    crust = 1
+
+    def toJSON(self):
+        json = "{\n  \"pizza\": {\n    \"size\": " + str(self.size) +\
+                ",\n    \"crust\": " + str(self.crust) +\
+                ",\n    \"name\": \"" + self.name +"\""\
+                ",\n    \"toppings\": ["
+        for i in range(len(self.toppings)):
+            json += "\n      " + str(self.toppings[i]).lower()
+            if (i < len(self.toppings)-1):
+                json += ","
+        json += "\n    ]\n  }\n}"
+        return json
+
+def main():
+    pizza = Pizza()
+    print(pizza.toJSON())
+
+main()
